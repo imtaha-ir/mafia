ad9f2b8980342d5105fc7c80f8c7b209003e837e
Ashkan-T <ashkanap629@gmail.com>
2025-07-26 12:41:20 +0330

    #25

--- src/pages/static/Roles.tsx
+++ src/pages/static/Roles.tsx
@@ -1,3 +1,18 @@
+import { Card, Grid, Typography } from "@mui/material";
+import RolesList from "../../data/static/Roles";
+
 export default function RolesPage() {
-  return <>Roles Guide</>;
+  return (
+    <Grid textAlign={"right"} container spacing={1} direction={"row-reverse"}>
+      {RolesList.map((role) => (
+        <Grid size={{ xs: 12, sm: 6, lg: 4, xl: 3 }}>
+          <Card>
+            <Typography variant="h5">{role.name}</Typography>
+            <Typography variant="subtitle1">{role.side}</Typography>
+            <Typography variant="subtitle1">{role.desc}</Typography>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
