1e43fc6dae2f3bbb9364c651a73e4ecaa1f41806
Amir Hossein <amirbbxplayer@gmail.com>
2025-08-04 02:43:51 +0330

    tash-52

--- src/pages/player-management/AddPayer.tsx
+++ src/pages/player-management/AddPayer.tsx
@@ -1,3 +1,28 @@
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 export default function AddPlayerPage() {
-  return <>New User</>;
+  return (
+    <>
+      <Stack direction="row" spacing={2}>
+        <Avatar alt="?" src="?" />
+      </Stack>
+      <Box component="form" noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="نام و نام خوانوادگی"
+          variant="outlined"
+        />
+      </Box>
+      <Box component="form" noValidate autoComplete="off">
+        <TextField id="outlined-basic" variant="outlined" type="date" />
+      </Box>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">ذخیره</Button>
+      </Stack>
+    </>
+  );
 }
