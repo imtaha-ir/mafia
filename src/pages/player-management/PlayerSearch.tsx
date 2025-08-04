edccfc85475e85314ddc7bcebd29c397ab4d64d4
Nazanin-009 <nazaninsgh9@gmail.com>
2025-08-05 02:04:48 +0330

    task-54

--- src/pages/player-management/PlayerSearch.tsx
+++ src/pages/player-management/PlayerSearch.tsx
@@ -1,3 +1,24 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { useState, type SetStateAction } from 'react';
 export default function PlayerSearchComponent() {
-  return <>Search</>;
+  const [input,setinput]=useState("");
+  const fetchData = (value: any) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=> response . json())
+    .then((json)=>{
+      console.log(json)
+     });
+  };
+  const handlechange=(value:any)=>{
+    setinput(value);
+    fetchData(value) 
+  }
+  return (
+    <div>
+    <SearchIcon/>
+    <input placeholder='Type to search...'
+     value={input}
+     onChange={(e)=> handlechange(e.target.value)}/> 
+    </div>
+  )
 }
