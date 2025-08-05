2d5773b76971241fd245f608038ecf8707da9d04
Nazanin-009 <nazaninsgh9@gmail.com>
2025-08-06 00:01:38 +0330

    task-54 changes

--- src/pages/player-management/PlayerSearch.tsx
+++ src/pages/player-management/PlayerSearch.tsx
@@ -1,33 +1,40 @@
-import SearchIcon from "@mui/icons-material/Search";
-import { useState, type SetStateAction } from "react";
-import { usePlayerContext } from "../../data/contexts/players";
-import { Box, Card } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import { useState, type SetStateAction } from 'react';
+import { usePlayerContext } from '../../data/contexts/players';
+import { Box, Card, IconButton, InputBase, Paper } from '@mui/material';
 export default function PlayerSearchComponent() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const playerDB = usePlayerContext();
-  const allPlayers = playerDB.list;
-
-  const handlechange = (value: any) => {
+  const [searchQuery,setSearchQuery]=useState("");
+  const playerDB=usePlayerContext()
+  const allPlayers=playerDB.list
+  const handlechange=(value:any)=>{
     setSearchQuery(value);
-  };
+  }
   return (
     <div>
-      <SearchIcon />
-      <input
-        placeholder="Type to search..."
+    <Paper
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="جستجو..."
         value={searchQuery}
-        onChange={(e) => handlechange(e.target.value)}
+        onChange={(e)=> handlechange(e.target.value)} 
+      
       />
-      <Box>
-        {allPlayers
-          .filter((player) => {
-            if (!searchQuery) return true;
-            return player.name.includes(searchQuery);
-          })
-          .map((player) => (
-            <Card>{player.name}</Card>
-          ))}
-      </Box>
+      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+          </Paper>
+   
+     <Box>
+      {allPlayers.filter((player) => {
+      if (!searchQuery) return true;
+      return player. name. includes(searchQuery);
+      })
+      .map((player) => (
+      <Card>{player.name}</Card>
+      ))}
+    </Box>
     </div>
-  );
-}
+  )
+}   
