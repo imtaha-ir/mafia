bc74f313fab1baa810c0affbf71373665990b452
Taha <imtaha.ir@gmail.com>
2025-08-05 10:50:09 +0330

    Review changes

--- src/pages/player-management/AddPayer.tsx
+++ src/pages/player-management/AddPayer.tsx
@@ -3,30 +3,61 @@ import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { usePlayerContext } from "../../data/contexts/players";
+import { useState } from "react";
 
 export default function AddPlayerPage() {
+  const playerDB = usePlayerContext();
+  const [name, setName] = useState<string>();
+  const [yearOfBirth, setYearOfBirth] = useState<number>(1360);
+  const showMessage = (msg: string) => {
+    alert(msg);
+  };
   const handleClick = () => {
-    console.log("hello");
+    if (name) {
+      const addedPlayer = playerDB.add({ name });
+      if (addedPlayer) {
+        showMessage("Saved");
+        setName("");
+        setYearOfBirth(1360);
+      } else {
+        showMessage("Player exists");
+      }
+    } else {
+      showMessage("Fill out all inputs");
+    }
   };
   return (
     <>
-      <Stack direction="row" spacing={2}>
-        <Avatar alt="?" src="?" />
-      </Stack>
-      <Box component="form" noValidate autoComplete="off">
-        <TextField
-          id="outlined-basic"
-          label="نام و نام خوانوادگی"
-          variant="outlined"
-        />
-      </Box>
-      <Box component="form" noValidate autoComplete="off">
-        <TextField id="outlined-basic" variant="outlined" type="date" />
-      </Box>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" onClick={handleClick}>
-          ذخیره
-        </Button>
+      <Stack spacing={3} alignItems="center">
+        <Avatar alt="?" src="?" sx={{ width: "200px", height: "200px" }} />
+
+        <Stack spacing={1}>
+          <TextField
+            id="outlined-basic"
+            label="نام و نام خوانوادگی"
+            variant="outlined"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            type="number"
+            value={yearOfBirth}
+            onChange={(e) => {
+              setYearOfBirth(Number(e.target.value));
+            }}
+          />
+        </Stack>
+        <Box>
+          <Button variant="contained" onClick={handleClick}>
+            ذخیره
+          </Button>
+        </Box>
       </Stack>
     </>
   );
