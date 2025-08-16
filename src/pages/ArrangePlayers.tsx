6a500520dfbf0dde80be56cc52e51d0bd37c2323
Amir Hossein <amirbbxplayer@gmail.com>
2025-08-16 18:17:14 +0330

    dialog

--- src/pages/ArrangePlayers.tsx
+++ src/pages/ArrangePlayers.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@mui/material";
+import PlayerSearchDialog from "../components/PlayerSearchDialog";
+import { useState } from "react";
+
 export default function ArrangePlayers() {
-  return <>ArrangePlayers</>;
+  const [dialogopen, setDialogOpen] = useState<boolean>(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setDialogOpen(true);
+        }}
+      >
+        Add
+      </Button>
+      <PlayerSearchDialog
+        open={dialogopen}
+        onExit={() => {
+          setDialogOpen(false);
+        }}
+      />
+    </>
+  );
 }
