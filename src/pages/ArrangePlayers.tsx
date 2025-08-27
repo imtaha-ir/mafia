4f0a73c35c228073d1b012bd8b320d2cfce88d57
abolfazl-faraji <farajiabolfazl99@gmail.com>
2025-08-27 16:41:03 +0330

    task-65

--- src/pages/ArrangePlayers.tsx
+++ src/pages/ArrangePlayers.tsx
@@ -1,6 +1,7 @@
 import {
   Avatar,
-  Button,
+  Box,
+  Card,
   Grid,
   IconButton,
   List,
@@ -9,24 +10,25 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Typography,
+  Stack,
+  TextField,
 } from "@mui/material";
 import PlayerSearchDialog from "../components/PlayerSearchDialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Player } from "../types/player.type";
 import { useGame } from "../data/contexts/game";
 import { useScreen } from "../data/contexts/screen";
 import { Add, Delete } from "@mui/icons-material";
 
 export default function ArrangePlayers() {
-  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
-  const [arrangedPlayers, setArrangedPlayer] = useState<Player[]>([]);
   const game = useGame();
   const screen = useScreen();
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [arrangedPlayers, setArrangedPlayer] = useState<Player[]>([]);
+  const [gameName, setGameName] = useState<string>("");
+
   function addPlayerToGame(newPlayer: Player): void {
-    const addedPlayer = arrangedPlayers.find(
-      (player) => player.id === newPlayer.id
-    );
+    const addedPlayer = arrangedPlayers.find((player) => player.id === newPlayer.id);
     if (addedPlayer) {
       screen.showMessage("بازیکن قبلا انتخاب شده");
     } else {
@@ -40,19 +42,62 @@ export default function ArrangePlayers() {
     setArrangedPlayer(newList);
   }
 
+  function getDateAndTime() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = currentDate.getDate().toString().padStart(2, "0");
+    const hours = currentDate.getHours().toString().padStart(2, "0");
+    const minuters = currentDate.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minuters} ${year}-${month}-${day}`;
+  }
+
+  function isEverythingOk() {
+    if (gameName.trim() !== "" && arrangedPlayers.length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function saveAndGotoNextPage() {
+    if (isEverythingOk()) {
+      game.createNewGame();
+      // TODO: change game name to {gameName}
+      game.addPlayerToCurrentGame(arrangedPlayers);
+      // TODO: navigate to next page
+    } else if (gameName.trim() === "") {
+      screen.showMessage("یک نام برای بازی بنویسید.");
+    } else if (arrangedPlayers.length < 5) {
+      screen.showMessage("بازی با حداقل ۵ بازیکن شروع میشه!");
+    }
+  }
+  useEffect(() => {
+    setGameName(getDateAndTime());
+  }, []);
   return (
     <>
+      <Box my={1}>
+        <Card>
+          <Stack my={3} mx={6}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="نام بازی"
+              value={gameName}
+              onChange={(e) => {
+                setGameName(e.target.value);
+              }}
+            />
+          </Stack>
+        </Card>
+      </Box>
       <List sx={{ width: "100%" }}>
         {arrangedPlayers.map((player, pIndex) => (
           <ListItem
             key={pIndex}
             sx={{ mt: 1, bgcolor: "background.paper" }}
             secondaryAction={
-              <IconButton
-                edge="end"
-                aria-label="delete"
-                onClick={() => removePlayerFromGame(pIndex)}
-              >
+              <IconButton edge="end" aria-label="delete" onClick={() => removePlayerFromGame(pIndex)}>
                 <Delete />
               </IconButton>
             }
@@ -61,10 +106,7 @@ export default function ArrangePlayers() {
             <ListItemAvatar>
               <Avatar />
             </ListItemAvatar>
-            <ListItemText
-              primary={player.name}
-              secondary={player.dateOfBirth}
-            />
+            <ListItemText primary={player.name} secondary={player.dateOfBirth} />
           </ListItem>
         ))}
         <ListItem disablePadding sx={{ mt: 1, bgcolor: "background.paper" }}>
