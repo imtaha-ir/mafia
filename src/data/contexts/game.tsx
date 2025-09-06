37b25a1d11a37d18df7d6a0a89efcb6e1df1bff2
Ashkan-T <ashkanap629@gmail.com>
2025-09-06 06:05:35 +0330

    Add loadLastGame to useEffect

--- src/data/contexts/game.tsx
+++ src/data/contexts/game.tsx
@@ -108,17 +108,8 @@ export const GameProvider: React.FC<{ children: React.ReactNode }> = ({
    * Load stored games from localStorage on mount.
    */
   useEffect(() => {
-    const allGames = getStoredGames();
-    setStoredGames(allGames);
-
-    if (allGames.length > 0) {
-      const lastGame = allGames.reduce((lastGameSelected, currentGame) =>
-        currentGame.lastPlay > lastGameSelected.lastPlay
-          ? currentGame
-          : lastGameSelected
-      );
-      setCurrentGame(lastGame);
-    }
+    setStoredGames(getStoredGames());
+    loadLastGame();
   }, []);
 
   /**
