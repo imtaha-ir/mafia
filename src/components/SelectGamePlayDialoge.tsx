d2b9ecad76514c7cd9045e4879cb3631425d5a23
Taha <imTaha.ir@gmail.com>
2025-09-22 21:43:42 +0330

    remove players from props and get it from game context

--- src/components/SelectGamePlayDialoge.tsx
+++ src/components/SelectGamePlayDialoge.tsx
@@ -1,64 +1,73 @@
-import { Avatar, Button, Dialog, DialogActions, DialogContent, DialogTitle, List, ListItemAvatar, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import type { Role } from "../types/roles.types";
-import { useState, type JSXElementConstructor, type ReactElement, type ReactNode, type ReactPortal } from "react";
-import type { GamePlayer } from "../types/player.type";
+import { useState } from "react";
+import { useGame } from "../data/contexts/game";
 
-
-
-interface SelectGamePlayDialogeProps{
-    open:boolean;
-    role:Role;
-    mode:"alive"|"dead";
-    onConfirm:(playerId:number)=>void;
-    onClose:()=>void;
-    players:GamePlayer[];
+interface SelectGamePlayDialogeProps {
+  open: boolean;
+  role: Role;
+  mode: "alive" | "dead";
+  onConfirm: (playerId: number) => void;
+  onClose: () => void;
 }
-   
 
-  
-export default function SelectGamePlayDialoge({open,role,mode,onConfirm,onClose,players}:SelectGamePlayDialogeProps){
-    const [selectedPlayerId,setSelectedPlayerId]=useState<number|null>(null)
-   const handleConfirm = ()=>{
-    if (selectedPlayerId !== null) {onConfirm(selectedPlayerId);
-        setSelectedPlayerId(null)
+export default function SelectGamePlayDialoge({
+  open,
+  role,
+  mode,
+  onConfirm,
+  onClose,
+}: SelectGamePlayDialogeProps) {
+  const [selectedPlayerId, setSelectedPlayerId] = useState<number | null>(null);
+  const game = useGame();
+
+  const handleConfirm = () => {
+    if (selectedPlayerId !== null) {
+      onConfirm(selectedPlayerId);
+      setSelectedPlayerId(null);
+      onClose();
     }
-   };
-   const filteredPlayers=players.filter(
-    (p)=>(mode ==="alive" ? p.alive:!p.alive)
-   );
-   
-    return(
-    
+  };
+  const filteredPlayers = game
+    .getGamePlayers()
+    .filter((p) => (mode === "alive" ? p.alive : !p.alive));
 
-        <Dialog open={open}  onClose={onClose} fullWidth>
-            <DialogTitle>
-                {role.name}:بازیکنی را انتخاب کنید 
-            </DialogTitle>
-               <DialogContent>
-                <List>
-                {filteredPlayers.map((player)=>(
-                    <ListItemButton key={player.id} selected={selectedPlayerId===player.id}
-                    onClick={()=>setSelectedPlayerId(player.id)}>
-                        <ListItemAvatar>
-                        <Avatar src={player.avatar}>
-                            {player.name[0]}
-                        </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={player.name}/>
-                    </ListItemButton>
-                ))}
-                </List>
-               </DialogContent>
-                    <DialogActions>
-                        <Button onClick={onClose}>لغو</Button>
-                        <Button
-                        onClick={handleConfirm}
-                        disabled={selectedPlayerId===null}
-                        >
-                        تأیید
-                        </Button>
-                    </DialogActions>
-
-        </Dialog>
-    )
+  return (
+    <Dialog open={open} onClose={onClose} fullWidth>
+      <DialogTitle>{role.name}، بازیکنی را انتخاب کنید</DialogTitle>
+      <DialogContent>
+        <List>
+          {filteredPlayers.map((player) => (
+            <ListItemButton
+              key={player.id}
+              selected={selectedPlayerId === player.id}
+              onClick={() => setSelectedPlayerId(player.id)}
+            >
+              <ListItemAvatar>
+                <Avatar src={player.avatar}>{player.name[0]}</Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={player.name} />
+            </ListItemButton>
+          ))}
+        </List>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>لغو</Button>
+        <Button onClick={handleConfirm} disabled={selectedPlayerId === null}>
+          تأیید
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
